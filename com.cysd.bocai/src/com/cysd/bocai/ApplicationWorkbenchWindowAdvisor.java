904f2e75690434148dee8775720738d51b659d60
Li <416959261@qq.com>
2016-06-25 10:57:40 +0800

    关闭程序时，弹出确认框，选择是否保存数据

--- com.cysd.bocai/src/com/cysd/bocai/ApplicationWorkbenchWindowAdvisor.java
+++ com.cysd.bocai/src/com/cysd/bocai/ApplicationWorkbenchWindowAdvisor.java
@@ -1,6 +1,11 @@
 package com.cysd.bocai;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
@@ -23,5 +28,24 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		configurer.setShowCoolBar(false);
 		configurer.setShowStatusLine(false);
 		configurer.setTitle("RCP Application");
+	}
+	/**
+	 * 关闭程序前，弹出确认框
+	 */
+	@Override
+	public boolean preWindowShellClose() {
+		IWorkbench workbench = getWindowConfigurer().getWindow().getWorkbench();
+		Shell shell = new Shell(workbench.getDisplay());
+	    MessageBox messageBox = new MessageBox(shell, SWT.ICON_QUESTION
+	            | SWT.YES | SWT.NO | SWT.CANCEL);
+	    messageBox.setMessage("是否保存内容");
+	    messageBox.setText("关闭程序");
+	    int response = messageBox.open();
+	    if(response == SWT.CANCEL){
+	    	return false;
+	    }else if (response == SWT.YES){
+	    	//数据存储
+	    }
+	    return true;
 	}
 }
